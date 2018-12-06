0e262b6c186824ea1c8e0d72f814840111dc8963
董皓 <donghaocode@163.com>
2018-12-06 14:33:37 +0800

    提交

---
+++ okkk/okkk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  okkk
+//
+//  Created by 董皓 on 2018/12/6.
+//  Copyright © 2018 董皓. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ okkk/okkk/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  okkk
+//
+//  Created by 董皓 on 2018/12/6.
+//  Copyright © 2018 董皓. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
